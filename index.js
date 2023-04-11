1714783637db171d1b3383ffb59323eaae13d928
Johnmef <72965249+Johnmef@users.noreply.github.com>
2023-04-11 16:08:19 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const heading = document.getElementById('heading');
 
 setTimeout(()=>{
-  heading.innerHTML = 'This is heading';
+  heading.innerHTML = 'This is the heading';
   navigator.vibrate(1000)
 },2000)
